f3b072ee26a97598aac8f8e3321b297a97a25b39
notskm <41784014+notskm@users.noreply.github.com>
2024-03-14 01:06:49 -0400

    Check rules 36, 37, 38, 41, 42, 43, 44
    The rules may be found at:
    https://www.cs.colostate.edu/~pouchet/classes/CS453/hw25-minijava/cs453/doc/miniJava-typesystem.pdf

---
+++ src/main/java/picojava/TypeCheckSimp.java
@@ -0,0 +1,60 @@
+package picojava;
+
+import java.util.HashMap;
+
+import syntaxtree.*;
+import visitor.GJDepthFirst;
+
+public class TypeCheckSimp extends GJDepthFirst<String, HashMap<String, String>> {
+
+    public String visit(NodeChoice n, HashMap<String, String> argu) {
+        return n.choice.accept(this, argu);
+    }
+
+    public String visit(Expression n, HashMap<String, String> argu) {
+        return n.f0.accept(this, argu);
+    }
+
+    public String visit(PrimaryExpression n, HashMap<String, String> argu) {
+        return n.f0.accept(this, argu);
+    }
+
+    public String visit(IntegerLiteral n, HashMap<String, String> argu) {
+        return "Int";
+    }
+
+    public String visit(TrueLiteral n, HashMap<String, String> argu) {
+        return "Boolean";
+    }
+
+    public String visit(FalseLiteral n, HashMap<String, String> argu) {
+        return "Boolean";
+    }
+
+    public String visit(Identifier n, HashMap<String, String> argu) {
+        return n.f0.toString();
+    }
+
+    public String visit(ArrayAllocationExpression n, HashMap<String, String> argu) {
+        final String type = n.f3.accept(this, argu);
+
+        if (type.equals("Int")) {
+            return "Int[]";
+        } else {
+            return "error";
+        }
+    }
+
+    public String visit(NotExpression n, HashMap<String, String> argu) {
+        final String type = n.f1.accept(this, argu);
+        if (type.equals("Boolean")) {
+            return type;
+        } else {
+            return "error";
+        }
+    }
+
+    public String visit(BracketExpression n, HashMap<String, String> argu) {
+        return n.f1.accept(this, argu);
+    }
+}
---
+++ src/test/java/picojava/TypeCheckSimpTest.java
@@ -0,0 +1,127 @@
+package picojava;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import syntaxtree.*;
+
+public class TypeCheckSimpTest {
+    private TypeCheckSimp check;
+    private HashMap<String, String> emptySymbolTable;
+
+    @BeforeEach
+    public void setup() {
+        check = new TypeCheckSimp();
+        emptySymbolTable = new HashMap<>();
+    }
+
+    @Test
+    public void testTypeOfIntegerLiteralIsInt() {
+        final NodeToken token = new NodeToken("1");
+        final IntegerLiteral tl = new IntegerLiteral(token);
+        final String type = tl.accept(check, emptySymbolTable);
+        assertEquals("Int", type);
+    }
+
+    @Test
+    public void testTypeOfTrueLiteralIsBoolean() {
+        final TrueLiteral tl = new TrueLiteral();
+        final String type = tl.accept(check, emptySymbolTable);
+        assertEquals("Boolean", type);
+    }
+
+    @Test
+    public void testTypeOfFalseLiteralIsBoolean() {
+        final FalseLiteral tl = new FalseLiteral();
+        final String type = tl.accept(check, emptySymbolTable);
+        assertEquals("Boolean", type);
+    }
+
+    @Test
+    public void testIdentifierTypeIsNameOfIdentifier() {
+        final NodeToken nt = new NodeToken("MyType");
+        final Identifier id = new Identifier(nt);
+
+        final String ret = id.accept(check, emptySymbolTable);
+
+        assertEquals("MyType", ret);
+    }
+
+    @Test
+    public void testArrayAllocationExpressionIsOfTypeIntArrayWhenGivenAnInt() {
+        final NodeToken nt = new NodeToken("1");
+        final IntegerLiteral il = new IntegerLiteral(nt);
+        final NodeChoice nc = new NodeChoice(il);
+        final PrimaryExpression pe = new PrimaryExpression(nc);
+        final NodeChoice nc2 = new NodeChoice(pe);
+        final Expression e = new Expression(nc2);
+        final ArrayAllocationExpression aae = new ArrayAllocationExpression(e);
+
+        final String type = aae.accept(check, emptySymbolTable);
+
+        assertEquals("Int[]", type);
+    }
+
+    @Test
+    public void testArrayAllocationExpressionProducesErrorWhenNotGivenAnInt() {
+        final TrueLiteral tl = new TrueLiteral();
+        final NodeChoice nc = new NodeChoice(tl);
+        final PrimaryExpression pe = new PrimaryExpression(nc);
+        final NodeChoice nc2 = new NodeChoice(pe);
+        final Expression e = new Expression(nc2);
+        final ArrayAllocationExpression aae = new ArrayAllocationExpression(e);
+
+        final String type = aae.accept(check, emptySymbolTable);
+
+        assertEquals("error", type);
+    }
+
+    @Test
+    public void testNotExpressionIsOfTypeBooleanWhenExpressionIsOfTypeBoolean() {
+        final TrueLiteral tl = new TrueLiteral();
+        final NodeChoice nc = new NodeChoice(tl);
+        final PrimaryExpression pe = new PrimaryExpression(nc);
+        final NodeChoice nc2 = new NodeChoice(pe);
+        final Expression e = new Expression(nc2);
+        final NotExpression ne = new NotExpression(e);
+
+        final String type = ne.accept(check, emptySymbolTable);
+
+        assertEquals("Boolean", type);
+    }
+
+    @Test
+    public void testNotExpressionProducesErrorWhenExpressionIsNotBoolean() {
+        final NodeToken nt = new NodeToken("1");
+        final IntegerLiteral il = new IntegerLiteral(nt);
+        final NodeChoice nc = new NodeChoice(il);
+        final PrimaryExpression pe = new PrimaryExpression(nc);
+        final NodeChoice nc2 = new NodeChoice(pe);
+        final Expression e = new Expression(nc2);
+        final NotExpression ne = new NotExpression(e);
+
+        final String type = ne.accept(check, emptySymbolTable);
+
+        assertEquals("error", type);
+    }
+
+    @Test
+    public void testBracketExpressionIsSameTypeAsExpression() {
+        final NodeToken token = new NodeToken("1");
+        final IntegerLiteral il = new IntegerLiteral(token);
+        final NodeChoice nc = new NodeChoice(il);
+        final PrimaryExpression pe = new PrimaryExpression(nc);
+        final NodeChoice nc2 = new NodeChoice(pe);
+        final Expression e = new Expression(nc2);
+        final BracketExpression be = new BracketExpression(e);
+
+        final String bracketType = be.accept(check, emptySymbolTable);
+        final String expressionType = e.accept(check, emptySymbolTable);
+
+        assertEquals(expressionType, bracketType);
+    }
+}
