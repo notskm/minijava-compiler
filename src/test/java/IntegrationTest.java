f3e2fe9a84c207dbfd128f8926bf35ade7b0a485
notskm <41784014+notskm@users.noreply.github.com>
2024-04-01 15:04:13 -0400

    Test compilation to Vapor

--- src/test/java/IntegrationTest.java
+++ src/test/java/IntegrationTest.java
@@ -1,11 +1,15 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import minijava.MethodTableVis;
+import minijava.MiniJavaToVaporVis;
 import minijava.SymTableVis;
 import minijava.TypeCheckSimp;
 import syntaxtree.Node;
@@ -40,7 +44,7 @@ public class IntegrationTest {
 
     @ParameterizedTest
     @ValueSource(strings = { "Factorial", "BinaryTree", "BubbleSort", "LinearSearch", "LinkedList", "QuickSort",
-            "TreeVisitor", "TreeVisitor", "MoreThan4", "NoOverloading" })
+            "TreeVisitor", "TreeVisitor", "MoreThan4" })
     public void testTypecheckingInvalidPrograms(String file) {
         final String filename = "programs/java/" + file + "-error.java";
         try (FileInputStream fStream = new FileInputStream(filename);) {
@@ -58,5 +62,48 @@ public class IntegrationTest {
         } catch (Exception e) {
             fail(e.getMessage());
         }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "Factorial" })
+    public void testConversionToVaporValidPrograms(String file) {
+        final String filename = "programs/java/" + file + ".java";
+        final String vaporFilename = "programs/vapor/" + file + ".vapor";
+        try (
+                FileInputStream fStream = new FileInputStream(filename);
+                FileInputStream vapor = new FileInputStream(vaporFilename);) {
+            MiniJavaParser.ReInit(fStream);
+
+            final Node root = MiniJavaParser.Goal();
+            final SymTableVis<Void, Integer> vis = new SymTableVis<>();
+            final TypeCheckSimp check = new TypeCheckSimp();
+
+            root.accept(vis, 0);
+            root.accept(check, vis.symt);
+
+            final MethodTableVis methodTableVis = new MethodTableVis();
+            root.accept(methodTableVis);
+
+            final MiniJavaToVaporVis m2v = new MiniJavaToVaporVis(methodTableVis.methodTables);
+            root.accept(m2v, vis.symt);
+
+            final String output = m2v.toVapor();
+
+            assertEquals(streamToString(vapor), output);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    private String streamToString(InputStream stream) {
+        byte[] bytes = new byte[0];
+
+        try {
+            bytes = stream.readAllBytes();
+        } catch (IOException e) {
+
+        }
+
+        return new String(bytes);
     }
 }
