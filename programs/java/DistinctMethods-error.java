b9dfc59b847d842d25f167e34727b6ca9dda354d
notskm <41784014+notskm@users.noreply.github.com>
2024-04-03 19:48:12 -0400

    Test for distinct methods

---
+++ programs/java/DistinctMethods-error.java
@@ -0,0 +1,15 @@
+class DistinctMethods {
+    public static void main(String[] args) {
+
+    }
+}
+
+class A {
+    public int a() {
+        return 1;
+    }
+
+    public int a() {
+        return 1;
+    }
+}
